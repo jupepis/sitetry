5f9f09f2d68be5115e5528ebb714800f9c361391
Giuseppe Arena <g.arena@tilburguniversity.edu>
2022-02-15 10:47:52 +0100

    other dumb functions added

---
+++ R/otherfunctions.R
@@ -0,0 +1,116 @@
+#' addition_1
+#' 
+#' dumb function
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @export
+addition_1 <- function(x = NULL) {
+    if(is.null(x)){
+        stop("'x' cannot be NULL")
+    }
+    else{
+        return(sum(x))
+    }
+}
+
+#' addition_2
+#' 
+#' dumb function
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @export
+addition_2 <- function(x = NULL) {
+    if(is.null(x)){
+        stop("'x' cannot be NULL")
+    }
+    else{
+        return(sum(x))
+    }
+}
+
+
+#' addition_3
+#' 
+#' dumb function
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @export
+addition_3 <- function(x = NULL) {
+    if(is.null(x)){
+        stop("'x' cannot be NULL")
+    }
+    else{
+        return(sum(x))
+    }
+}
+
+
+#' addition_4
+#' 
+#' dumb function
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @export
+addition_4 <- function(x = NULL) {
+    if(is.null(x)){
+        stop("'x' cannot be NULL")
+    }
+    else{
+        return(sum(x))
+    }
+}
+
+#' addition_5
+#' 
+#' dumb function
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @export
+addition_5 <- function(x = NULL) {
+    if(is.null(x)){
+        stop("'x' cannot be NULL")
+    }
+    else{
+        return(sum(x))
+    }
+}
+
+
+#' addition_6
+#' 
+#' dumb function
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @export
+addition_6 <- function(x = NULL) {
+    if(is.null(x)){
+        stop("'x' cannot be NULL")
+    }
+    else{
+        return(sum(x))
+    }
+}
